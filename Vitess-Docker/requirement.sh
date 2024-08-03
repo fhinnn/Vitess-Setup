52a02d4bbcce8e3e62b197e792bdc63c06467f1a
fhinnn <deltras12388@gmail.com>
2024-08-03 12:21:44 +0700

    feat: add docker-compose for docker installation

--- Vitess-Docker/requirement.sh
+++ Vitess-Docker/requirement.sh
@@ -1,3 +1,3 @@
 #install packages
 sudo apt-get update -y && sudo apt install docker.io docker-compose -y
-docker run fhinnn/vitess-demo
+docker run -d -p 14200:14200 -p 14201:14201 -p 15000:15000 -p 15100:15100 -p 15101:15101 -p 15102:15102 -p 15001:15001 -p 15991:15991 -p 15999:15999 -p 16000:16000 -it fhinnn/vitess-demo
